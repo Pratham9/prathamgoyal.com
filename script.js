1164e86b0cf643bd66b9ae9e3516d26d53988740
Pratham Goyal <goyalp09@pm.me>
2022-11-05 00:36:38 -0700

    initial

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.auto-type-item', {
+    strings: ["Hello!", "नमस्कार", "Salut!", "こんにちは！", "ਸਤਿ ਸ਼੍ਰੀ ਅਕਾਲ", "¡Hola!"],
+    typeSpeed: 80,
+    backDelay: 2000,
+    backSpeed: 80,
+    loop: true
+})
